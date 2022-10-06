635edd6263f38edbcb750e269db9904056a4352d
HoangOanh0711 <62228385+HoangOanh0711@users.noreply.github.com>
2022-10-06 14:10:25 +0700

    Xong đăng nhập + đăng ký

--- app/src/main/java/com/example/newsapp/quenmatkhau1.java
+++ app/src/main/java/com/example/newsapp/quenmatkhau1.java
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.hbb20.CountryCodePicker;
 
 public class quenmatkhau1 extends AppCompatActivity {
 
     TextView btn_taiday;
     Button btn_guiotp;
+
+    EditText sdt;
+
+    CountryCodePicker countryCodePicker;
+    ImageView img_check;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,42 @@ public class quenmatkhau1 extends AppCompatActivity {
 
         btn_taiday = findViewById(R.id.btn_taiday_quenmk);
         btn_guiotp = findViewById(R.id.btn_guiotp);
+        sdt = findViewById(R.id.ed_sdt_quenmk);
+        img_check = findViewById(R.id.img_check_quenmk);
+        countryCodePicker = findViewById(R.id.ccp_quenmk);
+
+        countryCodePicker.registerCarrierNumberEditText(sdt);
+        countryCodePicker.setPhoneNumberValidityChangeListener(new CountryCodePicker.PhoneNumberValidityChangeListener() {
+            @Override
+            public void onValidityChanged(boolean isValidNumber) {
+                if (isValidNumber) {
+                    img_check.setImageResource(R.drawable.ic_checkok);
+                } else {
+                    img_check.setImageResource(R.drawable.ic_checkx);
+                }
+            }
+        });
+        sdt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String input = editable.toString().trim();
+                if (input.length() > 0) {
+                    img_check.setVisibility(View.VISIBLE);
+                } else {
+                    img_check.setVisibility(View.GONE);
+                }
+            }
+        });
 
         btn_taiday.setOnClickListener(new View.OnClickListener() {
             @Override
