25004e9495359ac6b08609354739ab57744b149d
LT&CH <chauhuy0123@gmail.com>
2022-10-30 00:28:35 +0700

    up pass

--- app/src/main/java/com/example/newsapp/quenmatkhau3.java
+++ app/src/main/java/com/example/newsapp/quenmatkhau3.java
@@ -1,32 +1,53 @@
 package com.example.newsapp;
 
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class quenmatkhau3 extends AppCompatActivity {
 
     EditText ed_matkhau,ed_nhaplaimk;
     Button btn_capnhat;
 
+
+    String user, pass, Phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_quenmatkhau3);
+
+        user = getIntent().getStringExtra("User");
+        pass = getIntent().getStringExtra("Pass");
+        Phone = getIntent().getStringExtra("phone");
 
         khaibao();
 
         btn_capnhat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(quenmatkhau3.this, quenmatkhau4.class);
-                startActivity(intent);
+                CheckInternet checkInternet = new CheckInternet();
+
+                String _Phone = getIntent().getStringExtra("Phone");
+                String _newpass = ed_matkhau.getText().toString().trim();
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference("User");
+                reference.child(_Phone).child("pass").setValue(_newpass);
+                startActivity(new Intent(getApplicationContext(), quenmatkhau4.class));
+
+                //Intent intent = new Intent(quenmatkhau3.this, quenmatkhau4.class);
+                //startActivity(intent);
                 finish();
             }
         });
