b9758da8690717f3f32da5d5ee103bf9b7e1f9fc
HoangOanh0711 <62228385+HoangOanh0711@users.noreply.github.com>
2022-10-30 14:25:51 +0700

    quan ly dia phuong

--- app/src/main/java/com/example/newsapp/quanlydiaphuong.java
+++ app/src/main/java/com/example/newsapp/quanlydiaphuong.java
@@ -1,14 +1,45 @@
 package com.example.newsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.newsapp.Thoitiet.thoitiet;
+
 public class quanlydiaphuong extends AppCompatActivity {
+    LinearLayout btn_themdiaphuong;
+    ImageView btn_quaylai;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quanlydiaphuong);
+
+        khaibao();
+
+        btn_themdiaphuong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(quanlydiaphuong.this,chonthanhpho.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_quaylai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(quanlydiaphuong.this, thoitiet.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void khaibao() {
+        btn_themdiaphuong = findViewById(R.id.btn_themdiaphuong);
+        btn_quaylai = findViewById(R.id.img_quaylai_quanlydiaphuong);
     }
 }
