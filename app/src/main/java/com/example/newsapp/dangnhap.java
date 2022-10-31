cbbded2b465d2c62b7a0bed4c490f14cfb5e8dfd
cuonguit1104 <vominhcuong114@gmail.com>
2022-10-31 12:52:30 +0700

    đổi code sign gg nhưng vẫn đang bị lỗi không vào trang chủ

--- app/src/main/java/com/example/newsapp/dangnhap.java
+++ app/src/main/java/com/example/newsapp/dangnhap.java
@@ -1,5 +1,8 @@
 package com.example.newsapp;
 
+import static com.google.android.gms.auth.api.signin.GoogleSignIn.getSignedInAccountFromIntent;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,13 +13,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.hbb20.CountryCodePicker;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.google.android.gms.tasks.Task;
+
+import java.net.Authenticator;
 
 public class dangnhap extends AppCompatActivity {
 
@@ -25,6 +48,9 @@ public class dangnhap extends AppCompatActivity {
     GoogleSignInOptions gso;
     GoogleSignInClient gsc;
     ImageView btn_signin_gg;
+    private FirebaseAuth mAuth;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance()
+            .getReferenceFromUrl("https://newsapp-a5dc3-default-rtdb.firebaseio.com/");
 
 
     @Override
@@ -33,13 +59,29 @@ public class dangnhap extends AppCompatActivity {
         setContentView(R.layout.activity_dangnhap);
 
         khaibao();
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        //đăng nhập google
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        gsc = GoogleSignIn.getClient(this,gso);
+        btn_signin_gg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = gsc.getSignInIntent();
+                startActivityForResult(intent,1234);
+            }
+        });
+
+
+
+       /* gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         gsc = GoogleSignIn.getClient(this, gso);
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);*/
 
 
 
-        if (account != null)
+       /* if (account != null)
         {
             Log.e("Account", account.getEmail());
             navigateNext();
@@ -50,7 +92,7 @@ public class dangnhap extends AppCompatActivity {
             public void onClick(View view) {
                 signIn();
             }
-        });
+        });*/
 
         btn_quenmk.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,7 +122,58 @@ public class dangnhap extends AppCompatActivity {
         });
     }
 
-    private void signIn ()
+    // đăng nhập gg
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,@NonNull Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1234) {
+            Task <GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+                FirebaseAuth.getInstance().signInWithCredential(credential)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Log.e("account",account.getEmail());
+                                    finish();
+                                    Intent intent = new Intent(getApplicationContext(), trangchu.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(dangnhap.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+       /* GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null)
+        {
+            Log.e("account",user.getEmail());
+        }*/
+        if (user != null) {
+            Log.e("account",user.getEmail());
+            finish();
+            Intent intent = new Intent(dangnhap.this, trangchu.class);
+            startActivity(intent);
+        }
+    }
+
+
+
+
+
+    /*private void signIn ()
     {
         Intent signIntent = gsc.getSignInIntent();
         startActivityForResult(signIntent, 1000);
@@ -99,14 +192,14 @@ public class dangnhap extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-    }
+    }*/
 
-    private void navigateNext()
+    /*private void navigateNext()
     {
         finish();
         Intent intentNext = new Intent(dangnhap.this, trangchu.class);
         startActivity(intentNext);
-    }
+    }*/
 
     private void khaibao(){
         btn_signin_gg = findViewById(R.id.imageView4);
