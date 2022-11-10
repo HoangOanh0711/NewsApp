4020670eb12bb7bd4d0e6ab94e452749e3250c9b
HoangOanh0711 <62228385+HoangOanh0711@users.noreply.github.com>
2022-11-10 23:43:54 +0700

    update event lịch

--- app/src/main/java/com/example/newsapp/LichVanNien/lichvannien.java
+++ app/src/main/java/com/example/newsapp/LichVanNien/lichvannien.java
@@ -2,6 +2,7 @@ package com.example.newsapp.LichVanNien;
 
 import static java.time.format.DateTimeFormatter.ofPattern;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -11,6 +12,7 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.widget.CalendarView;
@@ -42,6 +44,14 @@ public class lichvannien extends AppCompatActivity {
         CalendarView calendarView = findViewById(R.id.cld_lichvannien_lich);
         IMG_lichvannien_back = findViewById(R.id.img_lvn_back);
 //        setMonthView();
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                String d=i2+"/"+(i1+1);
+                event(d);
+            }
+        });
     }
     //Hàm quay về màn hình trước
     public void backFromLichVanNien(View view){
@@ -55,6 +65,78 @@ public class lichvannien extends AppCompatActivity {
             startActivity(intent,options.toBundle());
         }else {
             startActivity(intent);
+        }
+    }
+
+    private void event(String date) {
+        switch (date) {
+            case "1/1":
+                Log.e("event","Tết Dương Lịch");
+                break;
+            case "14/2":
+                Log.e("event","Lễ tình nhân (Valentine)");
+                break;
+            case "3/2":
+                Log.e("event","Ngày thành lập Đảng Cộng sản Việt Nam");
+                break;
+            case "27/2":
+                Log.e("event","Ngày Thầy thuốc Việt Nam");
+                break;
+            case "8/3":
+                Log.e("event","Ngày Quốc tế Phụ nữ");
+                break;
+            case "20/3":
+                Log.e("event","Ngày Quốc tế Hạnh phúc");
+                break;
+            case "26/3":
+                Log.e("event","Ngày thành lập Đoàn Thanh niên Cộng sản Hồ Chí Minh");
+                break;
+            case "22/4":
+                Log.e("event","Ngày Trái đất");
+                break;
+            case "1/5":
+                Log.e("event","Quốc tế lao động");
+                break;
+            case "19/5":
+                Log.e("event","Ngày sinh của Chủ tịch Hồ Chí Minh");
+                break;
+            case "1/6":
+                Log.e("event","Ngày Quốc tế Thiếu nhi");
+                break;
+            case "28/6":
+                Log.e("event","Ngày Gia đình Việt Nam");
+                break;
+            case "27/7":
+                Log.e("event","Ngày Thương binh Liệt sĩ");
+                break;
+            case "19/8":
+                Log.e("event","Ngày Cách mạng tháng Tám thành công");
+                break;
+            case "2/9":
+                Log.e("event","Ngày Quốc khánh nước Cộng hoà Xã hội Chủ nghĩa Việt Nam");
+                break;
+            case "7/9":
+                Log.e("event","Ngày thành lập Đài Truyền hình Việt Nam");
+                break;
+            case "13/10":
+                Log.e("event","Ngày Doanh nhân Việt Nam");
+                break;
+            case "14/10":
+                Log.e("event","Ngày thành lập Hội Nông dân Việt Nam");
+                break;
+            case "20/10":
+                Log.e("event","Ngày thành lập Hội Phụ nữ Việt Nam");
+                break;
+            case "20/11":
+                Log.e("event","Ngày Nhà giáo Việt Nam");
+                break;
+            case "22/12":
+                Log.e("event","Ngày thành lập Quân đội Nhân dân Việt Nam");
+                break;
+            case "25/12":
+                Log.e("event","Lễ Giáng Sinh");
+                break;
+
         }
     }
 //    private void initWidgets()
